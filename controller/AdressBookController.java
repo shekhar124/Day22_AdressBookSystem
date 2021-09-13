583b8dd6680ad2611c8989e398eeaebc8e7bf929
rajashekhar <rajashekharpasikanti@gmail.com>
2021-09-13 17:31:57 +0530

    Add Contact

--- controller/AdressBookController.java
+++ controller/AdressBookController.java
@@ -1,8 +1,12 @@
 package com.bl.controller;
 
+import com.bl.service.AdressBookService;
+
 public class AdressBookController {
 	public static void main(String[] args) {
 		System.out.println("Welcome to AddressBook");
+		AdressBookService adressBookService = new AdressBookService();
+		adressBookService.createPerson();
 
 	}
 
--- service/AdressBookService.java
+++ service/AdressBookService.java
@@ -1,7 +1,40 @@
-package com.bl.service;   
+package com.bl.service;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import com.bl.model.AdressBookModel;
 
 public class AdressBookService {
+
+	static ArrayList<AdressBookModel> adressBookModels = new ArrayList<>();
+	public static Scanner scan = new Scanner(System.in);
+
+	// Add contact to AdressBook
 	
-	
+	public void createPerson() {
+		AdressBookModel adressBookModel = new AdressBookModel();
+		System.out.println("Enter Your First Name");
+		String firstName = scan.next();
+		System.out.println("Enter Your Last Name");
+		String lastName = scan.next();
+		System.out.println("Enter Your city Name");
+		String city = scan.next();
+		System.out.println("Enter Your state Name");
+		String state = scan.next();
+		System.out.println("Enter Your phone Number");
+		int phNo = scan.nextInt();
+		System.out.println("Enter Your zip");
+		int zip = scan.nextInt();
+		System.out.println("Enter Your email");
+		String email = scan.next();
+		
+		adressBookModel.setCity(firstName);
+		adressBookModel.setLastName(lastName);
+		adressBookModel.setPhoneNo(phNo);
+		adressBookModel.setState(state);
+		adressBookModel.setZipCode(zip);
+		adressBookModel.setCity(city);
+		adressBookModel.setEmailId(email);
+	}
 }
