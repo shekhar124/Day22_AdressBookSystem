1f6a02fb65d6eeae680cd9bdd808594f9166ae54
rajashekhar <rajashekharpasikanti@gmail.com>
2021-09-13 17:03:15 +0530

    Create Contant

--- controller/AdressBookController.java
+++ controller/AdressBookController.java
@@ -1,11 +1,9 @@
 package com.bl.controller;
 
-
-
 public class AdressBookController {
 	public static void main(String[] args) {
 		System.out.println("Welcome to AddressBook");
-		
+
 	}
 
 }
--- model/AdressBookModel.java
+++ model/AdressBookModel.java
@@ -1,6 +1,73 @@
 package com.bl.model;
 
 public class AdressBookModel {
-	
-	
+	String firstName;
+	String lastName;
+	int phoneNo;
+	String emailId;
+	String state;
+	String city;
+	int zipCode;
+
+	public String tostring() {
+		return "AddressBookModel [firstName=" + firstName + ", lastName=" + lastName + ", phoneNo=" + phoneNo
+				+ ", email=" + emailId + ", state=" + state + ", city=" + city + ", zip=" + zipCode + "]";
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getPhoneNo() {
+		return phoneNo;
+	}
+
+	public void setPhoneNo(int phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public int getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
+	}
+
 }
