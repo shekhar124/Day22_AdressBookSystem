0f36014d316d6ede6eb2b596e54d8c03a464739f
rajashekhar <rajashekharpasikanti@gmail.com>
2021-09-14 14:44:15 +0530

    Edit

--- controller/AdressBookController.java
+++ controller/AdressBookController.java
@@ -1,5 +1,7 @@
 package com.bl.controller;
 
+import java.util.Scanner;
+
 import com.bl.service.AdressBookService;
 
 public class AdressBookController {
@@ -7,7 +9,31 @@ public class AdressBookController {
 		System.out.println("Welcome to AddressBook");
 		AdressBookService adressBookService = new AdressBookService();
 		adressBookService.createPerson();
-
+		Scanner scan = new Scanner(System.in);
+		int choice = 0;
+		final int EXIT_VALUE=3;
+		while(choice !=EXIT_VALUE){
+			System.out.println("1.Add Person\n2.Update Person\n"+EXIT_VALUE+".Exit");
+			choice = scan.nextInt();
+			switch(choice) {
+			case 1:
+				adressBookService.createPerson();
+				adressBookService.print();
+				break;
+			case 2:
+				System.out.println("enter name of person to update");
+			String name = scan.next();
+			adressBookService.updatePerson(name);
+			adressBookService.print();
+			break;
+			case EXIT_VALUE :
+				System.out.println("exit");
+				break;
+				default:
+					System.out.println("Enter valid value");
+					break;
+			}
+		}
 	}
 
 }
--- service/AdressBookService.java
+++ service/AdressBookService.java
@@ -36,5 +36,82 @@ public class AdressBookService {
 		adressBookModel.setZipCode(zip);
 		adressBookModel.setCity(city);
 		adressBookModel.setEmailId(email);
+		
+		adressBookModels.add(adressBookModel);
 	}
-}
+	//edit person detils
+	
+	public void updatePerson(String name) {
+		int flag = 0;
+		for (AdressBookModel model : adressBookModels) {
+			if(model.getFirstName().equals(name)) {
+				flag = 1;
+				System.out.println("enter what to update(city/state/phone/zip):");
+				String update = scan.next();
+				switch (update) {
+				case "city": 
+					System.out.println("enter city name to update:");
+					String city = scan.next();
+					model.setCity(city);
+					break;
+				case "state":
+					System.out.println("enter the state to update:");
+					String state = scan.next();
+					model.setState(state);
+					break;
+				case "phone":
+					System.out.println("enter phone no to update");
+					int phone = scan.nextInt();
+					model.setPhoneNo(phone);
+					break;
+				case "zip":
+					System.out.println("entr zip code");
+					int zip = scan.nextInt();
+					model.setZipCode(zip);
+					break;
+					 
+					
+				default:
+					System.out.println("u entered romg");
+					break;
+				}
+				
+			}
+
+		}
+		if(flag==0)
+			System.out.println("u entred detiles rong");
+		else
+			System.out.println("sucessfully updated .tq");
+	}
+
+	public void print() {
+		for(AdressBookModel iteams : adressBookModels) {
+			System.out.println(iteams);
+		}
+		
+	}
+	
+		}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
